9a3d45fc2d44fb17ee1d89dfe4d0e98766ecc9f7
loveforu <gmlduslek@naver.com>
2023-10-13 01:46:35 +0900

    dd

--- apply.sh
+++ apply.sh
@@ -4,5 +4,4 @@ export account=`echo $1 | cut -c 1-3`
 
 cd root
 # terraform plan -var-file=../conf/$account/$1.tfvars
-# terraform ///apply -auto-approve -var-file=../conf/$account/$1.tfvars
-echo "123"
+ terraform apply -auto-approve -var-file=../conf/$account/$1.tfvars
--- destroy.sh
+++ destroy.sh
@@ -3,5 +3,5 @@
 export account=`echo $1 | cut -c 1-3`
 
 cd root
-echo "123"
-#terraform destroy -auto-approve -var-file=../conf/$account/$1.tfvars
+
+terraform destroy -auto-approve -var-file=../conf/$account/$1.tfvars
